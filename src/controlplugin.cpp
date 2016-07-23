8b4fe8028b73546f3cc249c3f0b2ecf9199e468c
Matt Buckley <bucklemp@usc.edu>
2016-07-22 17:29:06 -0700

    in progress build, this is just a save

--- src/controlplugin.cpp
+++ src/controlplugin.cpp
@@ -9,13 +9,23 @@
 // DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
 // MISUSING THIS SOFTWARE.
 // 
-// You are free to use/modify/distribute this file for whatever purpose!
-// -------------------------------------------------------------------
-//
-// This file was automatically created for V-REP release V3.3.1 Rev1 on May 17th 2016
+//Plugin author:
+//Matt Buckley,
+//University of Southern California
 
 #include <iostream>
+#include <map>
 #include <controlplugin.h>
+#include "sim_module_variables.h"
+#include "simulation_variables.h"
+#include "position_external.h"
+#include "mathconstants.h"
+#include "stabilizer_types.h"
+#include "replaced_methods.h"
+
+extern "C" {
+#include <ekf.h>
+}
 #ifdef _WIN32
 	#ifdef QT_COMPIL
 		#include <direct.h>
@@ -36,55 +46,308 @@
 LIBRARY vrepLib;
 
 
-// --------------------------------------------------------------------------------------
-// simExtBubble_create
-double pParam=2, iParam=0, dParam=0, vParam=-2, cumul=0, lastE=0, pAlphaE=0, pBetaE=0, psp2=0, psp1=0, prevEuler=0;
-float targetPos[3],pos[3],l[3],vx[3],vy[3],sp[3],euler[3],m[12],temp[3];
-double e,pv,thrust,alphaE,betaE,rotCorr,alphaCorr,betaCorr,retvals[4];
-int stack,d,targetObj;
-void PID_callback(SScriptCallBack* cb) {
+/*
+static control_t control;
+static controllerState_t controllerState;
+static setpoint_t setpoint;
+static sensorData_t sensorData;
+static state_t state;
+*/
+static void ekf_flip() {
+	struct ekf *ekf_temp = ekf_front;
+	ekf_front = ekf_back;
+	ekf_back = ekf_temp;
+}
+bool initialized = false; //probably unnecessary
+int stack, targetObj;
+float targetPos[3],temp[3],targetVel[3],targetAngVel[3],targetQuat[4],targetAccel[3],targetAngles[3];
+float targetMatrix[12];
+float v_last[3]; //we'll get a neater way to calculate acceleration once we have a dynamics model
+double retvals[4];
+double last_vicon = 0;
+bool run_vicon = true;
+bool positionExternalFresh = false;
+bool new_quad = false;
+quad_env_data quad_internals[max_quads];
+
+int id_to_handle[max_quads];
+std::map<int,int> handle_to_id;
+
+#define vicon_sim_hz 100.0
+#define vicon_sim_step 1.0/vicon_hz
+
+long int current_tick = 0;
+int total_quads = 0;
+int ATTITUDE_UPDATE_RATE;
+float ATTITUDE_UPDATE_DT;
+/*
+sensorsAcquire(&sensorData, tick);
+stateEstimator(&state, &sensorData, tick);
+commanderGetSetpoint(&setpoint, &state);
+  if (RATE_DO_EXECUTE(RATE_500_HZ, tick)) {
+      // Rate-controled YAW is moving YAW angle setpoint
+      if (setpoint.mode.yaw == modeVelocity) {
+         setpoint.attitude.yaw -= setpoint.attitudeRate.yaw/500.0;
+        while (setpoint.attitude.yaw > 180.0)
+          setpoint.attitude.yaw -= 360.0;
+        while (setpoint.attitude.yaw < -180.0)
+          setpoint.attitude.yaw += 360.0;
+      }
+
+      if (!setpoint.enablePosCtrl) {
+        setpoint.attitudeRate.yaw = 0;
+      }
+
+      positionControllerMellinger(&control, &state, &setpoint, &controllerState, 0.01);
+
+      trajectoryState_t trajectoryState;
+      trajectoryGetState(&trajectoryState);
+
+      if (!setpoint.enablePosCtrl) {
+        control.thrust = setpoint.thrust;
+      }
+
+      m_roll = control.roll;
+      m_pitch = control.pitch;
+      m_yaw = control.yaw;
+
+      if (control.thrust == 0
+          || ( setpoint.enablePosCtrl &&
+             ( !sensorData.valid
+              || trajectoryState == TRAJECTORY_STATE_IDLE)))
+      {
+        control.thrust = 0;
+        control.roll = 0;
+        control.pitch = 0;
+        control.yaw = 0;
+
+        // attitudeControllerResetAllPID();
+        positionControllerReset(&controllerState);
+        trajectorySetState(TRAJECTORY_STATE_IDLE);
+        setpoint.attitude.yaw = state.attitude.yaw;
+      }
+    }
+    powerDistribution(&control);
+    */
+
+#define limitThrust(VAL) limitUint16(VAL)
+
+
+
+//put this in a lib somewhere
+void commanderGetSetpoint(setpoint_t *setpoint, const state_t *state) {// update setpoint
+	trajectoryPoint_t goal;
+	//trajectoryGetCurrentGoal(&goal); lol no
+	setpoint->position.x = goal.x;
+	setpoint->position.y = goal.y;
+	setpoint->position.z = goal.z;
+	setpoint->velocity.x = goal.velocity_x;
+	setpoint->velocity.y = goal.velocity_y;
+	setpoint->velocity.z = goal.velocity_z;
+	setpoint->attitude.yaw = goal.yaw;
+	setpoint->attitudeRate.roll = goal.omega.x;
+	setpoint->attitudeRate.pitch = goal.omega.y;
+	setpoint->attitudeRate.yaw = goal.omega.z;
+	setpoint->mode.x = modeAbs;
+	setpoint->mode.y = modeAbs;
+	setpoint->mode.z = modeAbs;
+	setpoint->mode.roll = modeDisable;
+	setpoint->mode.pitch = modeDisable;
+	setpoint->mode.yaw = modeAbs;
+	setpoint->enablePosCtrl = true;
+}
+
+void sensorsAcquire(sensorData_t *sensors, const uint32_t tick) {
+    simGetObjectPosition(targetObj,-1,targetPos);
+	simGetObjectQuaternion(targetObj,-1,targetQuat);
+	simGetObjectOrientation(targetObj,-1,targetAngles);
+    simGetObjectVelocity(targetObj,targetVel,targetAngVel);
+    targetAngles[1] = -targetAngles[1];
+    targetAngVel[1] = -targetAngVel[1];
+    temp[0]=0;
+    temp[1]=0;
+    temp[2]=0;
+	simBuildMatrixQ(temp,targetQuat,targetMatrix); //get rotation matrix
+	simTransformVector(targetMatrix,targetAngVel);
+	for (int i=0;i<3;++i) { //replace acceleration calculations here with something more elegant later
+		targetAccel[i] = (targetVel[i]-v_last[i])/simGetSimulationTimeStep();
+	}
+	targetAccel[2] +=  9.81;
+	simTransformVector(targetMatrix,targetAccel); //transform to current coordinate frame
+    simGetObjectVelocity(targetObj,v_last,temp);
+	sensors->valid = true;
+	sensors->gyro.x = degrees(targetAngVel[0]);
+	sensors->gyro.y = degrees(targetAngVel[1]);
+	sensors->gyro.z = degrees(targetAngVel[2]);
+	sensors->acc.x = targetAccel[0]/GRAV;
+	sensors->acc.y = targetAccel[1]/GRAV;
+	sensors->acc.z = targetAccel[2]/GRAV;
+	//don't need to call positionExternalGetLastData when not looping vicon because data is preserved in the struct, I think
+	if (run_vicon || new_quad) {
+		positionExternalFresh = true;
+		sensors->position.x = targetPos[0];
+		sensors->position.y = targetPos[1];
+		sensors->position.z = targetPos[2];
+		/*sensors->quaternion.q0 = targetQuat[0];
+		sensors->quaternion.q1 = targetQuat[1];
+		sensors->quaternion.q2 = targetQuat[2];
+		sensors->quaternion.q3 = targetQuat[3];*/
+		struct vec ang = mkvec(targetAngles[0],targetAngles[1],targetAngles[2]);
+		struct quat dir = rpy2quat(ang); //iguess
+		sensors->quaternion.x = dir.x;
+		sensors->quaternion.y = dir.y;
+		sensors->quaternion.z = dir.z;
+		sensors->quaternion.w = dir.w;
+	}
+}
+
+void step_firmware(SScriptCallBack* cb) { //takes no args
+	sim_rate_dt = simGetSimulationTimeStep();
+	sim_rate_hz = (int)(1.0/sim_rate_dt+0.5); //rough round
+	current_tick += sim_rate_hz;
+	if (run_vicon || (current_tick>(last_vicon+vicon_sim_step))) { //basic vicon simulation
+		last_vicon = time;
+		run_vicon = true;
+	}
+	for (current_quad = 0; current_quad < total_quads; ++current_quad) {
+		set_context(&quad_data[current_quad].context);
+		//simulate sensors
+		//call state estimation
+		targetObj = id_to_handle[current_quad];
+		sensorsAcquire(&quad_internals[current_quad].sensorData,current_tick);
+		ATTITUDE_UPDATE_RATE = sim_rate_hz;
+		ATTITUDE_UPDATE_DT = sim_rate_dt;
+		stateEstimator(&state, &sensorData, current_tick);
+		commanderGetSetpoint(&setpoint, &state);
+		if (setpoint.mode.yaw == modeVelocity) {
+	 		setpoint.attitude.yaw -= setpoint.attitudeRate.yaw/sim_rate_hz;
+		while (setpoint.attitude.yaw > 180.0)
+			setpoint.attitude.yaw -= 360.0;
+		while (setpoint.attitude.yaw < -180.0)
+			setpoint.attitude.yaw += 360.0;
+		}
+		positionControllerMellinger(&control, &state, &setpoint, &controllerState, sim_rate_dt);
+		trajectoryState_t trajectoryState;
+		trajectoryGetState(&trajectoryState);
+		if (control.thrust == 0
+		  || ( setpoint.enablePosCtrl &&
+		     ( !sensorData.valid
+		      || trajectoryState == TRAJECTORY_STATE_IDLE)))
+		{
+			control.thrust = 0;
+			control.roll = 0;
+			control.pitch = 0;
+			control.yaw = 0;
+
+			// attitudeControllerResetAllPID();
+			positionControllerReset(&controllerState);
+			trajectorySetState(TRAJECTORY_STATE_IDLE);
+			setpoint.attitude.yaw = state.attitude.yaw;
+		}
+		save_context(&quad_data[current_quad].context);
+	}
+	run_vicon = false;
+}
+
+void register_quadrotor(SScriptCallBack* cb) {
+	new_quad = true;
+	int new_handle;
+	simGetStackInt32Value(stack,&new_handle);
+	simPopStackItem(stack,1);
+	quad_internals[total_quads].ekf_initialized = false;
+	quad_internals[total_quads].first_vicon = true;
+	sensorsAcquire(&quad_internals[total_quads].sensorData,current_tick);
+
+	//initialize other unique quad state stuffs
+
+	id_to_handle[total_quads] = new_handle;
+	handle_to_id[new_handle] = total_quads;
+	new_quad = false;
+	++total_quads;
+}
+/*
+
+void ekf_callback(SScriptCallBack* cb) { //soon to be defunct
 	stack = cb->stackID;
 	simGetStackInt32Value(stack,&targetObj);
 	simPopStackItem(stack,1);
-	simGetStackInt32Value(stack,&d);
-	simPopStackItem(stack,1);
-    simGetObjectPosition(targetObj,-1,targetPos);
-    simGetObjectPosition(d,-1,pos);
-    simGetObjectVelocity(d,l,temp);
-    e=(targetPos[2]-pos[2]);
-    cumul=cumul+e;
-    pv=pParam*e;
-    thrust=5.335+pv+iParam*cumul+dParam*(e-lastE)+l[2]*vParam;
-    lastE=e;
-    // Horizontal control: 
-    simGetObjectPosition(targetObj,d,sp);
-    simGetObjectMatrix(d,-1,m);
-    vx[0]=1;vx[1]=0;vx[2]=0; //ew
-    simTransformVector(m,vx);
-    vy[0]=0;vy[1]=1;vy[2]=0; //ew
-    simTransformVector(m,vy);
-    alphaE=(vy[2]-m[11]);
-    alphaCorr=0.25*alphaE+2.1*(alphaE-pAlphaE);
-    betaE=(vx[2]-m[11]);
-    betaCorr=-0.25*betaE-2.1*(betaE-pBetaE);
-    pAlphaE=alphaE;
-    pBetaE=betaE;
-    alphaCorr=alphaCorr+sp[1]*0.005+1*(sp[1]-psp2);
-    betaCorr=betaCorr-sp[0]*0.005-1*(sp[0]-psp1);
-    psp2=sp[1];
-    psp1=sp[0];
-    
-    // Rotational control:
-    simGetObjectOrientation(d,-1,euler);
-    rotCorr=euler[2]*0.1+2*(euler[2]-prevEuler);
-    prevEuler=euler[2];
-    retvals[0]=thrust*(1-alphaCorr+betaCorr+rotCorr);
-	retvals[1]=thrust*(1-alphaCorr-betaCorr-rotCorr);
-	retvals[2]=thrust*(1+alphaCorr-betaCorr+rotCorr);
-	retvals[3]=thrust*(1+alphaCorr+betaCorr-rotCorr);
-	simPushDoubleTableOntoStack(stack,retvals,4);
-}
 
+	if (!initialized)  {
+		//float init[] = {0, 0, 0, 1};
+		//ekf_init(ekf_back, init, init, init);
+		last_vicon_sim = simGetSimulationTime();
+	    simGetObjectPosition(targetObj,-1,targetPos);
+	    simGetObjectVelocity(targetObj,targetVel,temp);
+	    simGetObjectQuaternion(targetObj,-1,targetQuat);
+
+		simBuildMatrixQ(temp,targetQuat,targetMatrix);
+		simTransformVector(targetMatrix,targetVel);
+
+		ekf_init(ekf_back, targetPos, targetVel, targetQuat);
+		ekf_init(ekf_front, targetPos, targetVel, targetQuat);
+	    simGetObjectVelocity(targetObj,v_last,temp);
+	    initialized = true;
+	}
+	else {
+		//maths to get ekf-style data from sim
+	    simGetObjectPosition(targetObj,-1,targetPos);
+	    simGetObjectVelocity(targetObj,targetVel,targetAngVel);
+	    temp[0]=0;
+	    temp[1]=0;
+	    temp[2]=0;
+	    simGetObjectQuaternion(targetObj,-1,targetQuat);
+		simBuildMatrixQ(temp,targetQuat,targetMatrix); //get rotation matrix
+		simTransformVector(targetMatrix,targetAngVel);
+		for (int i=0;i<3;++i) {
+			targetAccel[i] = (targetVel[i]-v_last[i])/simGetSimulationTimeStep();
+		}
+		targetAccel[2] +=  9.81;
+		simTransformVector(targetMatrix,targetAccel); //transform to current coordinate frame
+	    simGetObjectVelocity(targetObj,v_last,temp);
+
+	    //run ekf
+		ekf_imu(ekf_back, ekf_front, targetAccel, targetAngVel, simGetSimulationTimeStep());
+		ekf_flip();
+
+		double time=simGetSimulationTime();
+		if (time>(last_vicon+vicon_sim_step)) { //basic vicon simulation
+			last_vicon = time;
+			ekf_vicon(ekf_back, ekf_front, targetPos, targetQuat);
+			ekf_flip();
+		}
+	    for (int i=0;i<4;++i) {
+	    	retvals[i] = 8;
+	    }
+	    //std::cout << "TARGET VELOCITY:" << std::endl;
+	    //std::cout << targetVel[0] << ", " << targetVel[1] << ", " << targetVel[2] << std::endl;
+	    //std::cout << "TARGET ACCEL:" << std::endl;
+	    //std::cout << targetAccel[0] << ", " << targetAccel[1] << ", " << targetAccel[2] << std::endl;
+	    //std::cout << "EKF POSITION:" << std::endl;
+	    //std::cout << ekf_back->pos.x << ", " << ekf_back->pos.y << ", " << ekf_back->pos.z << std::endl;
+	    //std::cout << "TRUE POSITION:" << std::endl;
+	    //std::cout << targetPos[0] << ", " << targetPos[1] << ", " << targetPos[2] << std::endl;
+		simPushDoubleTableOntoStack(stack,retvals,4);
+	}
+}
+*/
+/*
+void ekf_initialize(SScriptCallBack* cb) {
+	//initialized = false;
+	
+	stack = cb->stackID;
+	simGetStackInt32Value(stack,&targetObj);
+	//float init[] = {0, 0, 0, 1};
+	//ekf_init(ekf_back, init, init, init);
+    simGetObjectPosition(targetObj,-1,targetPos);
+    simGetObjectVelocity(targetObj,targetVel,temp);
+	for (int i=0;i<3;++i) {
+		v_last[i] = targetVel[i];
+	}
+    simGetObjectQuaternion(targetObj,-1,targetQuat);
+	ekf_init(ekf_back, targetPos, targetVel, targetQuat);
+	ekf_init(ekf_front, targetPos, targetVel, targetQuat);
+	
+}*/
 
 VREP_DLLEXPORT unsigned char v_repStart(void* reservedPointer,int reservedInt)
 { // This is called just once, at the start of V-REP.
@@ -103,7 +366,6 @@ VREP_DLLEXPORT unsigned char v_repStart(void* reservedPointer,int reservedInt)
 
 	std::string currentDirAndPath(curDirAndFile);
 	std::string temp(currentDirAndPath);
-
 #ifdef _WIN32
 	temp+="\\v_rep.dll";
 #elif defined (__linux)
@@ -136,8 +398,9 @@ VREP_DLLEXPORT unsigned char v_repStart(void* reservedPointer,int reservedInt)
 	}
 
 	// Register 4 new Lua commands:
-	simRegisterScriptCallbackFunction("simExtPidControl@v_repExtQuadControlPlugin","table_4 controlPluginHandle=(number self,number target)",PID_callback);
-
+	//simRegisterScriptCallbackFunction("simExtPidControl@v_repExtQuadControlPlugin","table_4 controlPluginHandle=(number self,number target)",PID_callback);
+	simRegisterScriptCallbackFunction("simExtCallEkf@v_repExtQuadControlPlugin","controlPluginHandle=(number self)",ekf_callback);
+	simRegisterScriptCallbackFunction("simExtStartEkf@v_repExtQuadControlPlugin","controlPluginHandle=(number self)",ekf_initialize);
 
 	return(8); // initialization went fine, we return the version number of this plugin (can be queried with simGetModuleName)
 	// version 1 was for V-REP versions before V-REP 2.5.12
@@ -161,7 +424,8 @@ VREP_DLLEXPORT void* v_repMessage(int message,int* auxiliaryData,void* customDat
 
     if(message == sim_message_eventcallback_simulationabouttostart)
     {
-    	pParam=2, iParam=0, dParam=0, vParam=-2, cumul=0, lastE=0, pAlphaE=0, pBetaE=0, psp2=0, psp1=0, prevEuler=0;
+    	//pParam=2, iParam=0, dParam=0, vParam=-2, cumul=0, lastE=0, pAlphaE=0, pBetaE=0, psp2=0, psp1=0, prevEuler=0;
+    	
     	//reset PID values
         //previousStopSimulationRequestCounter=-1;
     }
